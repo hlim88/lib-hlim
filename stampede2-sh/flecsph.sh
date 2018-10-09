27306260c8fcc036ae5f08de4914ce7a00c8eff1
Hyun Lim <hylim1988@gmail.com>
2018-10-09 11:05:56 -0600

    Add script for stampede2

---
+++ stampede2-sh/flecsph.sh
@@ -0,0 +1,9 @@
+#Working module for flecsph
+
+module purge
+module load intel/18.0.2
+module load impi/18.0.2
+module load python3/3.7.0
+module load cmake/3.10.2
+module load boost/1.68
+module load git/2.9.0
